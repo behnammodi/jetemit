f2614957a9c4d11b038eaca7091356afa3ffd672
Behnam Mohammadi <itten@live.com>
2019-08-30 19:38:52 +0430

    added refunds

--- index.test.js
+++ index.test.js
@@ -1,18 +1,18 @@
 const { on, once, emit, unsubscribeOf } = require("./index");
 
 const unsubscribeA = on("A", arg => {
-  if (arg === true) console.log("emit A 1", arg);
-  else throw "arg is incorrect";
+  console.assert(arg === true, "arg is incorrect");
+  console.log("emit A 1", arg);
 });
 
 const unsubscribeB = on("A", arg => {
-  if (arg === true) console.log("emit A 2", arg);
-  else throw "arg is incorrect";
+  console.assert(arg === true, "arg is incorrect");
+  console.log("emit A 2", arg);
 });
 
 on("B", arg => {
-  if (typeof arg === "number") console.log("emit B", arg);
-  else throw "arg is incorrect";
+  console.assert(typeof arg === "number", "arg is incorrect");
+  console.log("emit B", arg);
 });
 
 emit("A", true);
@@ -49,3 +49,22 @@ emit("logs", "some logA functions are unsubscribed except");
 
 unsubscribeOf("logs");
 emit("logs", "There is no function to run");
+
+const unsubscribeGetMoney1 = on("getmoney", () => {
+  return 1000;
+});
+
+const unsubscribeGetMoney2 = on("getmoney", () => {
+  return 2000;
+});
+
+const refunds = emit("getmoney");
+console.log("refunds", refunds);
+console.assert(refunds.length == 2, "refunds error");
+
+unsubscribeGetMoney1();
+unsubscribeGetMoney2();
+
+const refunds2 = emit("getmoney");
+console.log("refunds2", refunds2);
+console.assert(refunds2.length == 0, "refunds2 error");
